53d0f718fa969c53502f1911ec927de756a6cadd
erankamil <erankamil@gmail.com>
2020-06-13 18:04:28 +0300

    Add project files.

---
+++ Ex04.Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MainMenu : IClickedListener , IBackWasClickedLisenter
+    {
+        private readonly string r_MenuName = "Main Menu";
+        public MenuItem m_MainItem;
+        private MenuItem m_Prev;
+
+        public MainMenu()
+        {
+            m_MainItem = new MenuItem(r_MenuName, this);
+            m_MainItem.m_Items = new List<MenuItem>();
+            m_Prev = null;
+        }
+
+        void IClickedListener.WasClicked(MenuItem i_Item)
+        {
+            Ex02.ConsoleUtils.Screen.Clear();
+            if (i_Item.m_Items != null)
+            {
+                m_Prev = m_MainItem;
+                m_MainItem = i_Item;
+                i_Item.Show();
+            }
+            else
+            {
+                i_Item.m_Action.DoAction();
+                System.Threading.Thread.Sleep(2000);
+                Ex02.ConsoleUtils.Screen.Clear();
+            }
+        }
+
+        void IBackWasClickedLisenter.BackClicked()
+        {
+            if (m_MainItem.m_Text == r_MenuName)
+            {
+                m_MainItem = null;
+            }
+            else
+            {
+                m_MainItem = m_Prev;
+                Ex02.ConsoleUtils.Screen.Clear();
+            }
+        } 
+
+        public void Show()
+        {
+            while (m_MainItem != null)
+            {
+                m_MainItem.Show();
+            }
+        }
+    }
+    
+    public interface IClickedListener
+    {
+        void WasClicked(MenuItem i_Item);
+    }
+
+    public interface IBackWasClickedLisenter
+    {
+        void BackClicked();
+    }
+}
---
+++ Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MenuItem
+    {
+        private int m_ItemIndex;
+        public string m_Text;
+        public IAction m_Action;
+        public  List<MenuItem> m_Items;
+        private Notifier<IClickedListener> m_ClickNotifier;
+        private Notifier<IBackWasClickedLisenter> m_BackClickedNotifier;
+
+        public MenuItem(string i_Text, MainMenu i_MainListener)
+        {
+            m_Text = i_Text;
+            initializeListener(i_MainListener);
+        }
+
+        public MenuItem(string i_Text,int i_ItemIndex, IAction i_Action, MainMenu i_MainListener)
+        {
+            m_Text = i_Text;
+            m_ItemIndex = i_ItemIndex;
+            m_Action = i_Action;
+            initializeListener(i_MainListener);
+        }
+
+        public MenuItem(string i_Text, int i_ItemIndex, List<MenuItem> i_Items, MainMenu i_MainListener)
+        {
+            m_Text = i_Text;
+            m_ItemIndex = i_ItemIndex;
+            m_Items = i_Items;
+            initializeListener(i_MainListener);
+        }
+
+        private void initializeListener(MainMenu i_MainListener)
+        {
+            m_ClickNotifier = new Notifier<IClickedListener>();
+            m_ClickNotifier.AddListeners(i_MainListener);
+            m_BackClickedNotifier = new Notifier<IBackWasClickedLisenter>();
+            m_BackClickedNotifier.AddListeners(i_MainListener);
+        }
+
+        private void getItemChoice()
+        {
+            string choice;
+            do
+            {
+                Console.WriteLine(@"Please enter your choice(by index) or back\exit");
+                choice = Console.ReadLine();
+            }
+            while (!checkValidChoice(choice));
+
+            if (choice == "0")
+            {
+                doWhenBackClicked();
+            }
+            else
+            {// the menu tell his menuItem he was choosen
+                m_Items[int.Parse(choice) - 1].doWhenClicked(m_Items[int.Parse(choice) - 1], int.Parse(choice));
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("======{0}=======", m_Text);
+            foreach (MenuItem currItem in m_Items)
+            {
+                Console.WriteLine(currItem.m_ItemIndex.ToString() + ") " + currItem.m_Text);
+            }
+            Console.WriteLine("0) To go back/exit press 0");
+            getItemChoice();
+        }
+        
+        private bool checkValidChoice(string i_ChoiceStr)
+        {
+            bool isValid = true;
+           if(int.TryParse(i_ChoiceStr,out int res))
+            {
+                if(res < 0 || res > m_Items.Count)
+                {
+                    isValid = false;
+                    Console.WriteLine("Index out of range! Min {0} Max {1}",0 , m_Items.Count);
+                }
+            }
+           else
+            {
+                isValid = false;
+                Console.WriteLine("Choice must be index from the menu");
+            }
+            return isValid;
+        }
+
+        // MenuItem notifies the system(MainMenu) he was clicked
+        private void doWhenClicked(MenuItem i_Item, int i_ChoiceIndex)
+        {
+             m_ClickNotifier.NotifyAllListerners(i_Item, i_ChoiceIndex);
+        }
+
+        private void doWhenBackClicked()
+        {
+            m_BackClickedNotifier.NotifyAllListerners(this, 0);
+        }
+
+    }
+
+    public class Notifier<T>
+    {
+        private List<T> m_Listeners;
+
+        public void AddListeners(T i_Listener)
+        {
+            if(m_Listeners == null)
+            {
+                m_Listeners = new List<T>();
+            }
+            m_Listeners.Add(i_Listener);
+        }
+
+        public void RemoveListener(T i_Listener)
+        {
+            m_Listeners.Remove(i_Listener);
+        }
+
+        public void NotifyAllListerners(MenuItem i_Item,int indexChoice)
+        {
+            foreach(T listener in m_Listeners)
+            {
+                if(indexChoice != 0)
+                {
+                    (listener as IClickedListener).WasClicked(i_Item);
+                }
+                else
+                {
+                    (listener as IBackWasClickedLisenter).BackClicked();
+                }
+            }
+        }
+    }
+
+    public interface IAction
+    {
+        void DoAction();
+    }
+
+
+}
+
+
+
+
